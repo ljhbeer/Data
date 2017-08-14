11599b128d9e9aa2310fef0400ebba7e53d76d04
ljh_beer <ljh_beer@163.com>
2017-08-14 16:41:21 +0800

    optmize the copyfrom/copyto

--- copyfrom.bat
+++ copyfrom.bat
@@ -1,4 +1,4 @@
-echo "copy *.data readme.log from source\stock\Data\*.data     jhstock\readme.log">>un.txt
-#copy E:\Project\Source\stock\Data\*.data *.data
-#copy E:\Git\GitHome\Source\stock\jhstock\readme.log readme.log
-pause
+@echo "copy *.data readme.log from source\stock\Data\*.data     jhstock\readme.log"
+@copy E:\Project\Source\stock\Data\*.data *.data
+@copy E:\Git\GitHome\Source\stock\jhstock\readme.log readme.log
+@pause
--- copyto.bat
+++ copyto.bat
@@ -1,4 +1,4 @@
-echo "copy  *.data readme.log to source\stock\Data\*.data     jhstock\readme.log  ">>un.txt
-#copy *.data E:\Project\Source\stock\Data\*.data
-#copy readme.log E:\Git\GitHome\Source\stock\jhstock\readme.log
-pause
+@echo "copy  *.data readme.log to source\stock\Data\*.data     jhstock\readme.log  "
+@copy *.data E:\Project\Source\stock\Data\*.data
+@copy readme.log E:\Git\GitHome\Source\stock\jhstock\readme.log
+@pause
