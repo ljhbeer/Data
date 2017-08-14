5de72f396151307b29168c80971805e5109959bf
ljh_beer <ljh_beer@163.com>
2017-08-14 11:37:55 +0800

    change copyfiles to copyfrom.bat and copyto.bat

---
+++ copyfrom.bat
@@ -0,0 +1,4 @@
+echo "copy *.data readme.log from source\stock\Data\*.data     jhstock\readme.log">>un.txt
+#copy E:\Project\Source\stock\Data\*.data *.data
+#copy E:\Git\GitHome\Source\stock\jhstock\readme.log readme.log
+pause
